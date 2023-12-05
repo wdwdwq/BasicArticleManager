74bf69936840d4d1a91bcec1271c8a3d2f01f2a1
wdwdwq <qkenr7278@gmail.com>
2023-12-05 14:02:09 +0900

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String cmd = sc.nextLine();
+		
+		System.out.println("입력된 명령어)" + cmd);
+		
+		System.out.println("==프로그램 ==");
+		
+		sc.close();
+	}
+}
