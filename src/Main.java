418ad20318d0425002697373ea339b81d42b886b
wdwdwq <qkenr7278@gmail.com>
2023-12-06 12:38:54 +0900

    article list

--- src/Main.java
+++ src/Main.java
@@ -33,17 +33,30 @@ public class Main {
 				// lastArticleId를 증가시키고 마지막으로 생성된 게시물 번호를 출력
 
 				Article article = new Article(lastArticleId, title, body);
-
+				// 사용자로부터 제목 내용을 입력 받아 새로운 Article 객체를 생성 articles 리스트에 추가
+				//
 				articles.add(article);
 
-				System.out.println(lastArticleId + "번 게시물이 생성되었습니다");
-			} else if (cmd.equals("article list")) {
+				System.out.println(lastArticleId + "번 게시물이 생성되었습니다");// 각 게시물 고유한 ID 부
+			} else if (cmd.equals("article list")) {// 저장된 게시물 목록을 확인
 				if (articles.size() == 0) {
-					System.out.println("게시물이 존재하지 않습니다");
+					System.out.println("게시물이 존재하지 않습니다");// 게시물이 없는 경우 "게시물이 존재하지 않습니다" 라는 메시지를 출력
 					continue;
 				}
 				System.out.println("번호		/	제목");
-				for (int i = articles.size() -1; i >= 0; i--) {
+				for (int i = articles.size() - 1; i >= 0; i--) {// 리스트에 저장된 게시물을 역순으로 출력하여 번호와 제목을 표시
+					Article article = articles.get(i);
+					System.out.printf("%d		/	%s\n", article.id, article.title);
+
+				}
+
+			} else if (cmd.equals("article list")) {// 저장된 게시물 목록을 확인
+				if (articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다");// 게시물이 없는 경우 "게시물이 존재하지 않습니다" 라는 메시지를 출력
+					continue;
+				}
+				System.out.println("번호		/	제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {// 리스트에 저장된 게시물을 역순으로 출력하여 번호와 제목을 표시
 					Article article = articles.get(i);
 					System.out.printf("%d		/	%s\n", article.id, article.title);
 
@@ -64,12 +77,12 @@ public class Main {
 // 현재 article write 라는 명령어를 통해 생성된 게시물들을 실제로 저장할 수 있게 처리하고 
 //article list라는 명령어를 통해 저장되어 있는 게시물들의 목록을 볼 수 있게 
 //게시물을 저장해둔다 
-class Article {
+class Article {// 게시물 클래스
 	int id;
 	String title;
 	String body;
 
-	Article(int id, String title, String body) {
+	Article(int id, String title, String body) {// 생성자를 통해 게시물의 정보를 초기화할 수 있
 		this.id = id;
 		this.title = title;
 		this.body = body;
